1f5fdd797210470ebdcd913b4ea84ab09360bced
guzuchao <guzuchao@amazon.com>
2013-08-05 10:45:38 +0800

    add comment.

--- Example.java
+++ Example.java
@@ -2,14 +2,15 @@ import java.util.Arrays;
 
 public class Example {
     public static void main(final String... _) {
-        for (final String s : new Stream<String>(
+        for (final String s : new Stream<String>( // The template parameter need more design
+                                                  // does it need 2 parameter for input & return type?
                 new Stream<String>(_, new Processor<String>(){
                         String handle(final String s) {
                             return s + "ix";
                         }
                     }),
                     new Filter<String>(){
-                        Boolean handle(final String s) {
+                        Boolean handle(final String s) { // filte element
                             return "louix".equals(s);
                         }
                     })) {
